8ef6d89e75cf8e36f039ca56dc79a49d550aaa7d
yuan_xy <yuan_xin_yu@hotmail.com>
2019-12-19 17:51:11 +0800

    extract bool_t.py from types.py

--- canoser/__init__.py
+++ canoser/__init__.py
@@ -1,5 +1,5 @@
 from canoser.types import (  # noqa: F401
-    BoolT, ArrayT
+    ArrayT
 )
 from canoser.cursor import Cursor  # noqa: F401
 from canoser.struct import Struct  # noqa: F401
@@ -10,5 +10,6 @@ from canoser.tuple_t import TupleT  # noqa: F401
 from canoser.map_t import MapT  # noqa: F401
 from canoser.str_t import StrT  # noqa: F401
 from canoser.bytes_t import BytesT  # noqa: F401
+from canoser.bool_t import BoolT  # noqa: F401
 from canoser.int_type import Uint8, Uint16, Uint32, Uint64, Int8, Int16, Int32, Int64  # noqa: F401
 from canoser.util import bytes_to_int_list, hex_to_int_list  # noqa: F401
