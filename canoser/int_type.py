6ca80fb26d83a1bcd3c11bc20e7b10003e2b6d46
yuan_xy <yuan_xin_yu@hotmail.com>
2019-12-19 19:00:26 +0800

    improve performance by refactor int type

--- canoser/int_type.py
+++ canoser/int_type.py
@@ -1,94 +1,105 @@
 import struct
 
 class IntType:
-    _pack_map = {8: "B", 16: "H", 32: "L", 64: "Q"}
-
-    def __init__(self, bits, signed=False):
-        self.bits = bits
-        self.signed = signed
-
-    def __str__(self):
-        return self.__repr__()
-
-    def __repr__(self):
-        if self.signed:
-            return f"Int{self.bits}"
-        else:
-            return f"Uint{self.bits}"
 
     @classmethod
     def pretty_print_obj(cls, value, buffer, ident):
         buffer.write(f'{value}')
 
-    def pack_str(self):
-        endian = '<'
-        pack = IntType._pack_map[self.bits]
-        if self.signed:
-            pack = pack.lower()
-        return endian + pack
+    @classmethod
+    def encode(cls, value):
+        return struct.pack(cls.pack_str, value)
 
-    def encode(self, value):
-        return struct.pack(self.pack_str(), value)
+    @classmethod
+    def decode_bytes(cls, bytes):
+        return struct.unpack(cls.pack_str, bytes)[0]
 
-    def decode_bytes(self, bytes):
-        return struct.unpack(self.pack_str(), bytes)[0]
+    @classmethod
+    def decode(cls, cursor):
+        bytes = cursor.read_bytes(cls.byte_lens)
+        #return cls.decode_bytes(bytes) #inline decode_bytes
+        return struct.unpack(cls.pack_str, bytes)[0]
 
-    def decode(self, cursor):
-        bytes = cursor.read_bytes(int(self.bits / 8))
-        return self.decode_bytes(bytes)
-
-    @property
-    def max_value(self):
-        if self.signed:
-            return 2**(self.bits - 1) - 1
-        else:
-            return 2**(self.bits) - 1
-
-    @property
-    def min_value(self):
-        if self.signed:
-            return -2**(self.bits - 1)
-        else:
-            return 0
-
-    def int_unsafe(self, s):
+    @classmethod
+    def int_unsafe(cls, s):
         ret = int(s)
-        self.check_value(ret)
+        cls.check_value(ret)
         return ret
 
-    def int_safe(self, s):
+    @classmethod
+    def int_safe(cls, s):
         if isinstance(s, bool):
             raise TypeError(f"{s} is not a integer")
         if isinstance(s, int):
-            self.check_value(s)
+            cls.check_value(s)
             return s
         if not isinstance(s, str):
             raise TypeError(f"{s} is not instance of <str>.")
         if len(s) < 1:
             raise TypeError(f"'{s}' is empty.")
-        len_min = len(str(self.min_value))
-        len_max = len(str(self.max_value))
+        len_min = len(str(cls.min_value))
+        len_max = len(str(cls.max_value))
         if len(s) > max(len_min, len_max):
             raise TypeError(f"Length of {s} is larger than max:{max(len_min, len_max)}.")
         ret = int(s)
-        self.check_value(ret)
+        cls.check_value(ret)
         return ret
 
-    def check_value(self, value):
+
+    @classmethod
+    def check_value(cls, value):
         if not isinstance(value, int):
             raise TypeError(f"{value} is not instance of <int>.")
-        min, max = self.min_value, self.max_value
+        min, max = cls.min_value, cls.max_value
         if value < min or value > max:
             raise TypeError('value {} not in range {}-{}'.format(value, min, max))
 
+class Int8(IntType):
+    pack_str = "<b"
+    byte_lens = 1
+    max_value = 127
+    min_value = -128
 
-Int8 = IntType(8, True)
-Int16 = IntType(16, True)
-Int32 = IntType(32, True)
-Int64 = IntType(64, True)
+class Int16(IntType):
+    pack_str = "<h"
+    byte_lens = 2
+    max_value = 32767
+    min_value = -32768
 
-Uint8 = IntType(8)
-Uint16 = IntType(16)
-Uint32 = IntType(32)
-Uint64 = IntType(64)
+class Int32(IntType):
+    pack_str = "<l"
+    byte_lens = 4
+    max_value = 2147483647
+    min_value = -2147483648
+
+class Int64(IntType):
+    pack_str = "<q"
+    byte_lens = 8
+    max_value = 9223372036854775807
+    min_value = -9223372036854775808
+
+
+class Uint8(IntType):
+    pack_str = "<B"
+    byte_lens = 1
+    max_value = 255
+    min_value = 0
+
+class Uint16(IntType):
+    pack_str = "<H"
+    byte_lens = 2
+    max_value = 65535
+    min_value = 0
+
+class Uint32(IntType):
+    pack_str = "<L"
+    byte_lens = 4
+    max_value = 4294967295
+    min_value = 0
+
+class Uint64(IntType):
+    pack_str = "<Q"
+    byte_lens = 8
+    max_value = 18446744073709551615
+    min_value = 0
 
